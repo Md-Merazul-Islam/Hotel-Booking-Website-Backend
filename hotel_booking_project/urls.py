6b1aa0b5536d760dfccf00a07807eab7658db250
Md Merazul Islam <129538030+Md-Merazul-Islam@users.noreply.github.com>
2024-10-19 14:25:55 +0600

    hotel booking website backend code

---
+++ hotel_booking_project/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('user/', include('account.urls')),
+    path('hotel/', include('hotels.urls')),
+    path('payment/', include('payment.urls')),
+    
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
